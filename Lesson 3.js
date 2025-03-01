3c8316dce65ad3d7f49db75a823b24bf6b141f4a
Nissarr Alee <168177873+sajelanisar@users.noreply.github.com>
2025-03-01 14:33:08 +0500

    Initial upload

---
+++ Lesson 3.js
@@ -0,0 +1,57 @@
+// Lesson 3: Data Types
+// JavaScript me 5 main data types hote hain:
+
+
+// Data Type	Example
+// String	"Hello"
+// Number	25, 3.14
+// Boolean	true, false
+// Array	["Apple", "Banana"]
+// Object	{ name: "Ali", age: 18 }
+
+//  Example
+let message = "Welcome to JavaScript!";
+let price = 199.99;
+let isStudent = true;
+let colors = ["Red", "Green", "Blue"];
+let person = { name: "Ali", age: 18 };
+
+console.log(message, price, isStudent, colors, person);
+
+// Tumhara Task (Practice Karo!) Task One
+//  Task 1: Console me apna naam aur umar print karo.
+//  Task 2: 3 favorite colors ko ek array me store karo.
+//  Task 3: Ek object banao jo tumhara name, age, aur city store kare.
+
+// Task for You! Task Two
+// 1️ Apne 3 favorite fruits ka ek array banao.
+// 2️ Console me 2nd fruit ko print karo.
+// 3️ Ek naya fruit add karo aur updated array print karo.
+
+//  Task for You: Task Three
+// 1️ Apne 3 favorite cities ka ek array banao.
+// 2️ Console me pehla city print karo.
+// 3️ Ek naya city add karo aur updated array print karo.
+
+//  Task for You: Task Four
+//  Task for You:
+// 1️ Apne 5 favorite foods ka ek array banao.
+// 2️ 3rd food ko console me print karo.
+// 3️ Ek naya food add karo aur updated array print karo.
+
+//  Task for You: Task Five
+// Task for You:
+// 1️ Ek 5 car names ka array banao.
+// 2️ 2nd car ko print karo.
+// 3️ Ek nayi car add karo aur updated array print karo.
+
+//  Task for You: Task Six
+// Task for You:
+// 1️ Apne 4 favorite games ka array banao.
+// 2️ 3rd game ko print karo.
+// 3️ Ek naya game add karo aur updated array print karo.
+
+//  Task for You:
+// 1️ Apne 3 favorite programming languages ka array banao.
+// 2️ 2nd language ko print karo.
+// 3️ Ek aur new language add karo aur updated array print karo.
