75cfe0badb0d7e29156f5fa6db5fa4dac217e4a3
Nissarr Alee <168177873+sajelanisar@users.noreply.github.com>
2025-03-01 14:20:06 +0500

    Update Lesson 1.js

--- Lesson 1.js
+++ Lesson 1.js
@@ -1,1 +1,32 @@
+//  Lesson 1: JavaScript Introduction
+// JavaScript ek programming language hai jo web pages ko interactive banata hai. Agar HTML aur CSS structure aur design dete hain, to JavaScript functionality add karta hai.
 
+// JavaScript Kaise Likhein?
+// 1HTML file me <script> tag ka use karke
+
+// Create HTML file and open in browser
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <title>JavaScript Test</title>
+// </head>
+// <body>
+//     <h1>JavaScript Start!</h1>
+
+//     <script>
+//         alert("Hello, JavaScript!");
+//     </script>
+// </body>
+// </html>
+
+
+// alert("Hello, JavaScript!"); ek pop-up message dikhayega.
+
+// 2 Alag JavaScript File (.js) me likhna
+
+// Ek new file banao script.js
+// HTML file me include karo
+<script src="script.js"></script>
+// Ab script.js file me likho:
+console.log("Hello, JavaScript!");
+// Yeh console me message print karega.
