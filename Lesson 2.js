76ac0f4b6281a35019ff66d6c3f9d9ec20ff5026
Nissarr Alee <168177873+sajelanisar@users.noreply.github.com>
2025-03-01 14:23:59 +0500

    Initial upload

---
+++ Lesson 2.js
@@ -0,0 +1,14 @@
+// Lesson 2: Variables (Data Store Karne ke liye)
+// Variables ka use data store aur reuse karne ke liye hota hai.
+var name = "Nisar";  // Old method (Avoid)
+let age = 18;        // Preferred method
+const country = "Pakistan";  // Value change nahi hoti
+//  Differences
+// var → Old method, avoid karo.
+// let → Value change ho sakti hai.
+// const → Value change nahi ho sakti.
+// 🔹 Example: Console me Print karna
+let name = "Ali";
+console.log("Mera naam hai " + name);  
+// Output:
+// Mera naam hai Ali
